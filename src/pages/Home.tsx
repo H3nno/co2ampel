e67ea00b413a8d7111e08693cd370db4019c6899
H3nno <henning.o.schmidt@icloud.com>
2022-05-17 16:14:43 +0200

    weeiter

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,24 @@
-function Home(){
+var ws281x = require('rpi-ws281x-native');
 
-    return(
-        <h1>
-            Home
-        </h1>
+function Home() {
+
+    return (
+        <div className="MainHome">
+            <h1 className="headline">
+                Home
+            </h1>
+            <button id="redbutton">Rote Farbe anstellen</button>
+            <button id="yellowbutton">Gelbe Farbe anstellen</button>
+            <button id="greenbutton">Grüne Farbe anstellen</button>
+            <div className="square">
+                <div className="circle"></div>
+                <div className="circle0"></div>
+                <div className="circle1"></div>
+            </div>
+
+        </div>
+
+
     )
 
 }
